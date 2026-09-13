820bd710f9ae65b9e09e64161eea38445090d605
fahmidamahi221-svg <fahmidamahi221@gmail.com>
2026-09-13 23:42:21 +0600

    Fix vite base path for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
-  base: '/b15-a05-fahmida/',
+  base: '/B15-A05-Fahmida/',
 })
